7d8d3395b9f3b0c13d3babb83d5e106ebf983016
Tom <tom.slaytor@gmail.com>
2021-12-24 14:57:51 +0200

    re-structured the data base to eliminate repetition - implemented diary and fixed bugs - re-commented code - begining refactoring process

--- final_project.py
+++ final_project.py
@@ -1,9 +1,9 @@
+from logging import currentframe
 import sqlite3
 import os
 import datetime
 import email
 from dotenv import dotenv_values, load_dotenv
-# from module import config
 from flask import Flask, render_template, request, url_for, redirect, session
 from flask_mail import Mail, Message
 from flask_session import Session
@@ -14,14 +14,16 @@ from itsdangerous.url_safe import URLSafeSerializer, URLSafeTimedSerializer
 # this is my own library
 from support import error, password_check, login_required, success
 
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# SECRET_KEY = os.environ.get("SECRET_KEY") # TODO FIX SECRET KEY
 SECRET_KEY = "SECRET_KEY"
-s = Serializer(SECRET_KEY)
-SECURITY_PASSWORD_SALT = "im_a_rocket_man"
+s = Serializer(SECRET_KEY) # TODO WHEN DO I USE S?
+SECURITY_PASSWORD_SALT = "im_a_rocket_man" # TODO - IMPROVE THE SECURITY OF SALT
 
-load_dotenv("/Users/thomas/final_project/PASSWORDS.env") 
 
-# create connection to SQL database, and create table if it doesn't exist
+load_dotenv("/Users/thomas/final_project/PASSWORDS.env") # TODO REMIND YOURSELF WHAT THIS DOES AGAIN
+
+# CREATE CONNECTION TO SQL DATABASE - AND CREATE TABLES
 connection = sqlite3.connect('final_project.db', check_same_thread=False)
 cursor = connection.cursor()
 
@@ -34,37 +36,57 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS users (
     confirmed boolean DEFAULT False
 )""")
 
-# create content table
+# CREATE CONTENT TABLE
 cursor.execute("""CREATE TABLE IF NOT EXISTS content (
     id integer PRIMARY KEY,
     title text NOT NULL
 )""")
 
-# create library table
+# CREATE LIBRARY TABLE
 cursor.execute("""CREATE TABLE IF NOT EXISTS library (
-    user_id integer FORIGN KEY references users(id),
-    content_id integer FORIGN KEY references content(id)
+    user_id integer, 
+    content_id integer,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (content_id) REFERENCES content(id)
 )""")
 
-# create diary table
-cursor.execute("""CREATE TABLE IF NOT EXISTS diary (
-    user_id integer FORIGN KEY references users(id),
-    content_id integer FORIGN KEY references content(id),
-    tempo integer NOT NULL CHECK (30 <= tempo <= 300),
-    rate integer NOT NULL CHECK (1 <= rate <= 5),
-    date DATE NOT NULL
+# CREATE RATING TABLE
+cursor.execute("""CREATE TABLE IF NOT EXISTS rating (
+    id INTEGER PRIMARY KEY,
+    rating TEXT NOT NULL
 )""")
+
+# CREATE DIARY TABLE
+cursor.execute("""CREATE TABLE IF NOT EXISTS diary (
+    id integer PRIMARY KEY,
+    user_id integer, 
+    content_id integer,
+    tempo integer NOT NULL CHECK (30 <= tempo <= 300),
+    rating_id integer NOT NULL,
+    notes TEXT,
+    date DATE NOT NULL, 
+    color TEXT NOTE NULL,
+    FOREIGN KEY (user_id) references users(id),
+    FOREIGN KEY (content_id) references content(id),
+    FOREIGN KEY (rating_id) REFERENCES rating(id)
+)""")
+
+# POPULATE THE RATING TABLE
+for n in range(1, 6):
+    rating = f"{n} stars"
+    cursor.execute("INSERT INTO rating (rating) VALUES (?)", (rating,))
+    connection.commit()
+
 
 app = Flask(__name__)
 
+# MAIL CONFIGURATION
 app.config['MAIL_SERVER']='smtp.googlemail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'musicpractice171@googlemail.com'
 app.config['MAIL_PASSWORD'] = os.environ["EMAIL_PASSWORD"]
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-
-
 
 mail = Mail(app)
 
@@ -74,27 +96,76 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# ************
+# START OF APP
+# ************
 
-
+# -----------
+# HOME ROUTE
+# -----------
 @app.route("/")
-
+# @login_required
 def home():
-    
     #####################################
     # HACK TO LET ME NOT LOGGIN EVERYTIME  # DELETE LATER
     session["user_id"] = 1
     session["verified"] = True
-    # HACK TO LET ME NOT LOGGIN EVERYTIME # DELETE LATER
+    # HACK TO LET ME NOT LOGGN EVERYTIME # DELETE LATER
     ######################################
    
     return render_template("index.html", user=session["user_id"])
 
+# ----------------------------------------------------
+# ROUTE - ADD NEW CONTENT TO CONTENT AND/OR LIBRARY TABLES
+# ----------------------------------------------------
+@app.route("/add_content", methods=["POST"])
+@login_required
+def add_content():
+    # GET THE INPUT FROM THE FORM
+    new_content = request.form.get("new_content").strip()
+    print("THIS IS NEW CONTENT ", new_content)
+    # CHECK IF THE CONTENT ALREADY EXISTS IN THE CONTENT TABLE
+    cursor.execute("SELECT * FROM content WHERE title = ?", (new_content,))
+    if cursor.fetchall():
+        print("THIS SHOULDN'T REACH HERE FOR TOTALLY NEW TITLES ")
+        cursor.execute("SELECT * FROM content WHERE title = ?", (new_content,))
+        content_id = cursor.fetchall()[0][0]
+        # CONTENT ALREADY IN CONTENT TABLE - NOW CHECKING IF CONTENT IN USER LIBRARY
+        cursor.execute("SELECT * FROM library WHERE content_id = ? AND user_id = ?", (content_id, session["user_id"]))
+        if cursor.fetchall():
+            # CONTENT ALREADY IN USER LIBRARY
+            return redirect("/diary")
+        else:
+            # CONTENT NOT IN USER LIBRARY - ADDING NOW
+            cursor.execute("INSERT INTO library (user_id, content_id) VALUES (?,?)", (session["user_id"], content_id))
+            connection.commit()
+            return redirect("/diary")
+    else:
+        # INSERT NEW CONTENT INTO CONTENT TABLE
+        cursor.execute("INSERT INTO content (title) VALUES (?)", (new_content,))
+        connection.commit()
+        # GET THE NEW CONTENT'S ID
+        cursor.execute("SELECT * FROM content WHERE title = ?", (new_content,))
+        content_id = cursor.fetchall()[0][0]
+        # ADD CONTENT TO USERS LIBRARY
+        cursor.execute("INSERT INTO library (user_id, content_id) VALUES (?, ?)", (session["user_id"], content_id) )
+        connection.commit()
+        return redirect("/diary")
+
+
+
+# ---------------------------------------------------------------------------------
+# DIARY ROUTE - DISPLAYS DROPDOWN OF USER'S LIBRARY AND TABLE OF USER'S DIARY TABLE
+# --------------------------------------------------------------------------------
 @app.route("/diary", methods=["GET", "POST"])
 @login_required
 def diary():
-    if request.method == "POST":
 
-        print('--- original order ---')
+    if request.method == "POST":
+        
+        # ####################################################################################################################################
+        # THIS WHOLE THING IS WIERD - LETS FIX ONE DAY
+        # ############################################
         # get all the values in one go
         a = {}
         a["content"] = request.form.get("content")
@@ -102,33 +173,81 @@ def diary():
         a["rating"] = request.form.get("rating")
         a["notes"] = request.form.get("notes")
 
-        cursor.execute("""INSERT INTO diary 
-            (date,
-            user_id,
-            content,
-            tempo,
-            rating,
-            notes) 
-        VALUES (DATE(), ?, ?, ?, ?, ?);""", (session["user_id"], a["content"], a["tempo"], a["rating"], a["notes"] ))
+        # EXTRACT RELEVANT INFO
+        # content_id
+        cursor.execute("SELECT id FROM content WHERE title = ?", (a["content"],))
+        content_id = cursor.fetchall()
+
+        # CHECK SELECTED CONTENT IS IN THE CONTENT TABLE
+        if not content_id:
+            return error("Problem with the selected content - something weird - I suggest deleting that content and re-uploading")
+        content_id = content_id[0][0]
+        
+        # tempo (as an int)
+        tempo = int(a["tempo"][:-4])
+        
+        # rating_id
+        rating_id = int(a["rating"][:-6])
+        # ############################################
+        # THIS WHOLE THING IS WIERD - LETS FIX ONE DAY
+        # ####################################################################################################################################
+
+        # # FIND THE LAST COLOR USED AND CHANGE IF NECESSARY
+        # cursor.execute("SELECT color FROM diary ORDER BY id DESC LIMIT 1")
+        # color = cursor.fetchall()
+        # if color:
+        #     # if DATE() != lastRecordedDate:
+        #     if 1 != 2:
+        #         if color[0] == "blue":
+        #             color[0] == "red"
+        #         else:
+        #             color[0] == "blue"
+        # else:
+        #     color.append("blue")
+        
+        # INSERT THE NEW VALUES INTO THE DIARY
+        cursor.execute("""INSERT INTO diary (date, user_id, content_id, tempo, rating_id, notes) 
+        VALUES (DATE(), ?, ?, ?, ?, ?)""", (session["user_id"], content_id, tempo, rating_id, a["notes"]))
         connection.commit()
 
-        print('--- sorted ---')
-
-        keys = request.form.keys()
-        keys = sorted(keys)
-
-        for key in keys:
-            #print(key, request.form[key])
-            print(key, request.form.get(key))
-
-        cursor.execute("SELECT title FROM content")
-        titles = cursor.fetchall()
-        return render_template("diary.html", TITLES=titles)
+       
+        # GET INFORMATION NEEDED FOR RENDER TEMPLATE
+        cursor.execute("""SELECT title FROM content 
+                            JOIN library ON content.id = library.content_id  
+                            WHERE library.user_id = ?""", (session["user_id"],))
+        CONTENT = cursor.fetchall()
+        
+        cursor.execute("""SELECT 
+                        content.title, diary.tempo, rating.rating, diary.notes, diary.color
+                        FROM diary 
+                        JOIN content ON content.id = diary.content_id 
+                        JOIN rating on rating.id = diary.rating_id
+                        ORDER BY diary.id DESC""")
+        DIARY = cursor.fetchall()
+     
+        # RETURN RENDER TEMPLATE
+        return render_template("diary.html", content=CONTENT, diary=DIARY,)
+    
     else:
-        cursor.execute("SELECT title FROM content")
-        titles = cursor.fetchall()
-        return render_template("diary.html", TITLES=titles)
+        # GET INFORMATION NEEDED FOR RENDER TEMPLATE
+        cursor.execute("""SELECT
+                        content.title, diary.tempo, rating.rating, diary.notes
+                        FROM diary 
+                        JOIN content ON content.id = diary.content_id 
+                        JOIN rating on rating.id = diary.rating_id
+                        ORDER BY diary.id DESC""")
+        DIARY = cursor.fetchall()
+        
+        cursor.execute("""SELECT title FROM content 
+                            JOIN library ON content.id = library.content_id  
+                            WHERE library.user_id = ?""", (session["user_id"],))
+        CONTENT = cursor.fetchall()
+        return render_template("diary.html", diary=DIARY, content=CONTENT)
 
+
+# -------------
+# ACCOUNT ROUTE
+# -------------
 @app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
@@ -138,7 +257,6 @@ def account():
             cursor.execute("UPDATE users SET user_name = ? WHERE id = ?", (request.form.get("change_username"), session["user_id"]))
             connection.commit()
             return redirect("/account")
-
         # update email
         elif request.form.get("update_email"):
             # check if email is available
@@ -170,6 +288,7 @@ def account():
             elif request.form.get("update_password") != request.form.get("confirm_update_password"):
                 # TODO change this error to javascript message
                 return error("Passwords do not match")
+                # TODO REIMPLEMENT PASSWORD CHECKS
             # elif not password_check(request.form.get("update_password"))[0]:
             #     return error("Password didn't pass the check")
             else:
@@ -185,6 +304,9 @@ def account():
         return render_template("account.html", user_name=USER)
 
 
+# --------------
+# ROUTE REGISTER
+# --------------
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -208,6 +330,7 @@ def register():
             if cursor.fetchall():
                 return error("email already in use, maybe you misspelled your email, or you already have an account?")
             else:
+                # TODO REIMPLEMENT PASSWORD CHECK
                 # check if password meets criteria
                 # valid = password_check(PASSWORD)
                 # if not valid[0]:
@@ -232,25 +355,28 @@ def register():
     else:
         return render_template("register.html")
 
-
+# ---------------------------------
+# ROUTE - CONFIRMS THE USER TOKEN
+# ---------------------------------
 @app.route('/confirm/<token>')
 def confirm_email(token):
-    print("HELLO!!!!!!!!: ", token)
+    # CHECK TOKEN IS VALID
     try:
         email = confirm_token(token)
-        print("EMAIL 1!!!!1!!!!!!: ", email)
         if not email:
             return error("Couldn't recognise email - try to login again and resend email when prompted")
     except:
         error ("The confirmation link is invalid or has expired")
     cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
     user = cursor.fetchall()[0]
-    print("USER!!!: ", user)
+    # CHECK IF ACCOUNT ALREADY CONFIRMED
+    # CONFIRMED
     if user[5] == 1:
         if session.get("user_id"):
             return error("Account already confirmed, and you are already logged in")
         else:
             return error("Account already confirmed. Please login")
+    # UNCONFIRMED
     else:
         cursor.execute("UPDATE users SET confirmed = 1 WHERE email = ?", (email,))
         connection.commit()
@@ -258,7 +384,9 @@ def confirm_email(token):
         session["verified"] = True
         return success("You have confirmed your account. Thanks")
 
-
+# ------------
+# LOGIN ROUTE
+# ------------
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
@@ -293,7 +421,6 @@ def resend():
     #  if session.get --- REDIRECT WITH ERROR MESSAGE IF THE SESSION[USER ID] IS INVALID SYNTAX
     email = cursor.execute("SELECT email FROM users WHERE id = ?", (session["user_id"],))
     email = email.fetchall()[0][0]
-    print("email is_______!!!!!!!!!!!!_________: ", email)
     token = generate_confirmation_token(email)
     confirm_url = url_for('confirm_email', token=token, _external=True)
     html = render_template('activate.html', confirm_url=confirm_url)
